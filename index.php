b6b4a2e64ad7aaddcf9d2e7600e59d1dd71bd5f9
Carlos <carlos@edukame.com>
2018-08-07 18:15:08 +0200

    Quitado navbar

--- index.php
+++ index.php
@@ -14,9 +14,6 @@
   <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">
     </a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
 
       <ul id="nav-mobile" class="sidenav">
         <li><a href="#">Navbar Link</a></li>
